dbcc81671d749594cd193b6ffde8ddf15597cc8e
Ankush Chatterjee <ankushchatterjee.iitkgp@gmail.com>
2016-05-21 16:23:53 +0530

    Cross validation framework added

--- v2.py
+++ v2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import os
 import glob
 import tensorflow as tf
@@ -20,51 +21,35 @@ def get_path(directory):
     return map(os.path.abspath, imgs), map(os.path.abspath, mask), map(os.path.abspath, gt)
 
 train, mask_train, gt_train =  get_path('../Data/DRIVE/training')
-test, mask_test, gt_test = get_path('../Data/DRIVE/test')
+test, mask_test, mask_gt = get_path('../Data/DRIVE/test')
+
 
 # Hyper Params
-total_patches = 60000
+
+data = pd.read_pickle('../Data/mean_normalised_df.pkl') 
+total_patches = len(data)
 num_training_images = len(train)
 patches_per_image = total_patches/num_training_images
-patch_dim = 31                          # Dimension of window used for training
-num_patches = 0                         # Patches used for training from the current image
-current_img_index = 0                   # Index of the current image in 'train'
-current_img = io.imread(train[0])    
-current_mask = img_as_float(io.imread(mask_train[0]))
-current_gt = img_as_float(io.imread(gt_train[0]))
+patch_dim = 31                         
+current_batch_ind = 0
 print patches_per_image
 
-# When we have extracted 'patches_per_image' number of patches from our current image
-# we call this function to change the current image
-def load_next_img(data,mask_data,gt_data):
-    global num_patches, current_img_index, current_img, current_mask, current_gt
-    num_patches = 0
-    current_img_index +=1
-    if current_img_index < len(data):
-        current_img = io.imread(data[current_img_index])                     # 0-255
-        current_mask = img_as_float(io.imread(mask_data[current_img_index])) # 0.0-1.0
-        current_gt = img_as_float(io.imread(gt_data[current_img_index]))     # 0.0-1.0
-    else:
-        print 'No more images left in set'
 
-# size should be a factor of patches_per_image    
-def next_batch(size, data, mask_data, gt_data):
-    global num_patches
-    count = 0
+#mean_img = pd.read_pickle('../Data/mean_img.pkl')
+def next_batch(size, df):
+    global current_batch_ind
+    flag = False
+    if current_batch_ind + size> len(df):
+        print 'Next batch cannot be called because of insufficient remaining data'
+        flag = True
     batch_x = np.zeros((size, patch_dim**2*3))
     batch_y = np.zeros((size,1), dtype = 'uint8')
-    while count < size:
-        i = np.random.randint(0,current_img.shape[0])
-        j = np.random.randint(0,current_img.shape[1])
-        h = (patch_dim - 1)/2
-        if int(np.sum(current_mask[i-h:i+h+1,j-h:j+h+1])/patch_dim**2) == 1:
-            batch_x[count] = np.reshape(current_img[i-h:i+h+1,j-h:j+h+1], -1)
-            batch_y[count] = int(current_gt[i,j])
-            count +=1
-    num_patches += size
-    if num_patches >= patches_per_image:
-        load_next_img(data, mask_data, gt_data)
-    return (batch_x, batch_y)
+    for i in range(current_batch_ind, current_batch_ind+size):
+        batch_x[i - current_batch_ind] = df.loc[i][:-1]
+        batch_y[i - current_batch_ind] = int(df.loc[i][patch_dim**2*3])
+    current_batch_ind += size
+    return (batch_x, batch_y), flag
+
 
 sess = tf.InteractiveSession()
 def weight_variable(shape):
@@ -87,15 +72,15 @@ def max_pool_2x2(x,pad_type=1):
                         strides=[1, 2, 2, 1], padding='VALID')   
 
 
-
 x = tf.placeholder(tf.float32, shape=[None, patch_dim**2*3])
 y_ = tf.placeholder(tf.float32, shape=[None, 1])
+
 
 
 W_conv1 = weight_variable([4, 4, 3, 64])
 b_conv1 = bias_variable([64])
 
-x_image = tf.reshape(x, [-1,patch_dim,patch_dim,3])
+x_image = tf.reshape(x, [-1,patch_dim,patch_dim,3]) # Check
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1,0) + b_conv1)
 
 
@@ -111,6 +96,7 @@ b_conv3 = bias_variable([64])
 
 h_conv3 = tf.nn.relu(conv2d(h_pool1, W_conv3) + b_conv3)
 h_pool2 = max_pool_2x2(h_conv3)
+
 
 
 fc_neurons = 512
@@ -134,22 +120,36 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_ind
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.initialize_all_variables())
 
 
 
-for i in range(600):
-    batch = next_batch(100,train, mask_train, gt_train)
-    if i%2 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x:batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %f"%(i, train_accuracy))
-        print("1 - %g"%(batch[1].mean()) )
+num_folds = 5
+subset_size = len(data) / 5
+
+for fold in range(num_folds):
+    current_batch_ind = 0
+    print 'Fold %d / %d'%(fold,num_folds)
+    test_data = data[fold*subset_size:(fold+1)*subset_size]
+    test_data = test_data.reset_index(drop = True)
+    
+    train_data = pd.concat([data[:fold*subset_size], data[(fold+1)*subset_size:]])
+    train_data = train_data.reset_index(drop = True)
+    
+    sess.run(tf.initialize_all_variables())
+    for i in range(16):
+        batch, empty = next_batch(30, train_data)
+        if empty:
+            break
+        if i%4 == 0:
+            train_accuracy = accuracy.eval(feed_dict={
+                x:batch[0], y_: batch[1], keep_prob: 1.0})
+            print("step %d, training accuracy %f"%(i, train_accuracy))
+            #print("1 - %g"%(batch[1].mean()) )
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-
-current_img_index = -1
-test_batch = next_batch(2000, test, mask_test, gt_test)
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: test_batch[0], y_: test_batch[1], keep_prob: 1.0}))
+    # if subset_size is too big, OOM error will occur
+    current_batch_ind = 0
+    cv_batch, _ = next_batch(subset_size, test_data)
+    print("test accuracy for fold %d = %g"%(fold+1, accuracy.eval(feed_dict={
+    x: cv_batch[0], y_: cv_batch[1], keep_prob: 1.0})))
 
