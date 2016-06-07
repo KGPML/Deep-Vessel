b598ec71b5dc13ec7f0fffb8935291c19a7625d9
Ankush Chatterjee <ankushchatterjee.iitkgp@gmail.com>
2016-06-07 12:18:52 +0530

    Optimisations

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,20 +1,30 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[18]:
 
 import numpy as np
 import pandas as pd
 from skimage import io, draw
 from skimage.util import img_as_float, img_as_ubyte
-get_ipython().magic(u'matplotlib inline')
+#%matplotlib inline
 import os
 import glob
+import time
 
 
-# In[4]:
+# In[19]:
 
 def get_path(directory):
+    """ Gets the filenames of all training, mask and ground truth images in the given 
+        directory 
+        Args:
+            directory: The path to the root folder
+        Output:
+            imgs: List of paths to files containing images
+            mask: List of paths to files containing masks of the images
+            gt:   List of paths to files containing corresponding ground truth images
+    """
     imgs = glob.glob(directory + '/images/*.tif')
     imgs.sort()
     #a = [x.split('/')[-1].split('.')[0] for x in train]
@@ -33,7 +43,7 @@ train, mask_train, gt_train =  get_path('../Data/DRIVE/training')
 test, mask_test, mask_gt = get_path('../Data/DRIVE/test')
 
 
-# In[27]:
+# In[20]:
 
 # Hyper Params
 total_patches = 600
@@ -48,11 +58,17 @@ current_gt = img_as_float(io.imread(gt_train[0]))
 positive_proprtion = 0.5
 
 
-# In[28]:
+# In[21]:
 
-# When we have extracted 'patches_per_image' number of patches from our current image
-# we call this function to change the current image
 def load_next_img(data,mask_data,gt_data):
+    """When we have extracted 'PATCHES_PER_IMAGE' number of patches from our 
+       current image we call this function to change the current image
+       Args:
+           data: The list of paths to the images
+           mask_data: List of paths to the corresponding masks of images
+           gt_data: List of paths to the corresponding ground truth images
+       
+    """
     global num_patches, current_img_index, current_img, current_mask, current_gt
     num_patches = 0
     if current_img_index < len(data)-1:
@@ -65,78 +81,65 @@ def load_next_img(data,mask_data,gt_data):
     else:
         print 'No more images left in set'
         return False
+
+
+# In[22]:
+
+begin = time.time()
+print "Creating DataFrame"
 df = pd.DataFrame(columns = np.arange(patch_dim**2*3+1))
+print "Dataframe ready"
+
+
+# In[23]:
 
 def save_img_data(data, mask_data, gt_data):
+    """Extracts PATCHES_PER_IMAGE number of patches from each image
+        
+       It maintains a count of positive and negative patches and maintains
+       the ratio POSITIVE_PROPORTION = pos/(pos+neg)
+       Args:
+           data: The list of paths to the images
+           mask_data: List of paths to the corresponding masks of images
+           gt_data: List of paths to the corresponding ground truth images
+       
+    """
     pos_count = 0
     neg_count = 0
     global df
-    # Fill up till one class reaches its limit
     while pos_count +neg_count < patches_per_image: 
+        # Choose a random point
         i = np.random.randint(patch_dim/2,current_img.shape[0]-patch_dim/2)
         j = np.random.randint(patch_dim/2,current_img.shape[1]-patch_dim/2)
         h = (patch_dim - 1)/2
         if int(np.sum(current_mask[i-h:i+h+1,j-h:j+h+1])/patch_dim**2) == 1:
             ind = current_img_index*patches_per_image+pos_count+neg_count
+            
+            # If a positive sample is found and positive count hasn't reached its limit
             if int(current_gt[i,j])==1 and pos_count < positive_proprtion*patches_per_image:
                 df.loc[ind] = np.arange(patch_dim**2*3+1)
                 df.loc[ind][0:-1] = np.reshape(current_img[i-h:i+h+1,j-h:j+h+1], -1)
                 df.loc[ind][patch_dim**2*3] = int(current_gt[i,j])
                 pos_count += 1
+            # If a negative sample is found and negative count hasn't reached its limit
             elif int(current_gt[i,j])==0 and neg_count < (1-positive_proprtion)*patches_per_image:
                 df.loc[ind] = np.arange(patch_dim**2*3+1)
                 df.loc[ind][0:-1] = np.reshape(current_img[i-h:i+h+1,j-h:j+h+1], -1)
                 df.loc[ind][patch_dim**2*3] = int(current_gt[i,j])
                 neg_count += 1
-        
-'''
-def save_img_data(data, mask_data, gt_data):
-    count = 0
-    #grid_length = np.floor(np.sqrt(patches_per_image))
-    #print 'grid length = %d'%grid_length
-    
-    row_step = col_step = int(np.ceil(np.sqrt(0.7*current_mask.shape[0]*current_mask.shape[1]/(1.0*patches_per_image))) )   
-    #print 'steps = %d'%row_step
-    h = (patch_dim-1)/2
-    i = h+1
-    while i<current_mask.shape[0]-h-1-row_step :
-        j = h+1
-        while j<current_mask.shape[1]-h+1-col_step and count<patches_per_image:
-            p = np.random.randint(0,row_step)
-            q = np.random.randint(0,col_step)
-            #print 'i = %d, j = %d, count = %d'%(i,j,count)
-            #print 'p= %d, q = %d'%(p,q)
-            if current_mask[i+p-h,j+q-h]==current_mask[i+p-h,j+q+h]== current_mask[i+p+h,j+q-h] == current_mask[i+p+h,j+q+h]>0.99: #To avoid floating point comparison
-                ind = current_img_index*patches_per_image+count
-                df.loc[ind] = np.arange(patch_dim**2*3+1)
-                df.loc[ind][0:-1] = np.reshape(current_img[i+p-h:i+p+h+1,j+q-h:j+q+h+1], -1)
-                df.loc[ind][patch_dim**2*3] = int(current_gt[i+p,j+q])
-                count +=1
-            
-            j+=row_step
-        i+=col_step
-    #print count
-    while count < patches_per_image:
-        i = np.random.randint(50,current_img.shape[0]-50)
-        j = np.random.randint(50,current_img.shape[1]-50)
-        h = (patch_dim - 1)/2
-        if current_mask[i-h,j-h]==current_mask[i-h,j+h]== current_mask[i+h,j-h] == current_mask[i+h,j+h]>0.99: #To avoid floating point comparison
-            ind = current_img_index*patches_per_image+count
-            df.loc[ind] = np.arange(patch_dim**2*3+1)
-            df.loc[ind][0:-1] = np.reshape(current_img[i-h:i+h+1,j-h:j+h+1], -1)
-            df.loc[ind][patch_dim**2*3] = int(current_gt[i,j])
-            count +=1
-'''
 
 
-# In[29]:
+# In[24]:
 
 while load_next_img(train, mask_train, gt_train):
+    start = time.time()
     save_img_data(train,mask_train, gt_train)
+    print "Time taken for this image = %f secs" %( (time.time()-start))
 
 
-# In[30]:
+# In[25]:
 
+print "Mean Normalising"
 last = len(df.columns) -1
 mean_img = np.mean(df)[:-1]
 labels = df[last]
@@ -144,41 +147,21 @@ mean_normalised_df = df - np.mean(df)
 mean_normalised_df[last] = labels
 
 
-# In[31]:
+# In[26]:
 
+print "Randomly shuffling the datasets"
 mean_normalised_df = mean_normalised_df.iloc[np.random.permutation(len(df))]
 mean_normalised_df = mean_normalised_df.reset_index(drop=True)
 
 
-# In[32]:
+# In[27]:
 
+print "Writing to pickle"
 mean_normalised_df.to_pickle('../Data/mean_normalised_df_no_class_bias.pkl')
 mean_img.to_pickle('../Data/mean_img_no_class_bias.pkl')
 
 
-# In[1]:
+# In[28]:
 
-from scipy import misc, ndimage
-
-
-# In[13]:
-
-im = misc.imread(train[0])
-type(im)
-
-
-# In[12]:
-
-ig = ndimage.imread(train[0])
-type(ig)
-
-
-# In[15]:
-
-
-
-
-# In[ ]:
-
-
+print "Total time taken = %f mins" %( (time.time()-begin)/60.0)
 
